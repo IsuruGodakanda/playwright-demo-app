c112526117ba79b2ea477ae71e8f357dd68ef045
IsuruHL <isurugodakandahl@gmail.com>
2022-05-20 12:33:26 +0530

    PDA-014-test-run-in-multiple-browsers

---
+++ test/multiple-browser/browsers.test.ts
@@ -0,0 +1,27 @@
+import { Browser, BrowserContext, Page, firefox } from "playwright";
+
+describe("Launch local browser", () => {
+  let browser: Browser;
+  let context: BrowserContext;
+  let page: Page;
+  beforeAll(async () => {
+    browser = await firefox.launch({
+      headless: false,
+      // channel: "msedge" // If this not suggest browsers please install playwright globaly
+      // executablePath: "C:/Program Files (x86)/Google/Chrome/Application/chrome.exe"
+    });
+    context = await browser.newContext();
+    page = await context.newPage();
+    await page.goto("https://letcode.in/");
+  });
+
+  test("Goto letcode and verify title as LetCode with Koushik", async () => {
+    const title = await page.title();
+    console.log(title);
+    expect(title).toBe("LetCode with Koushik");
+  });
+
+  afterAll(async () => {
+    await browser.close();
+  });
+});
